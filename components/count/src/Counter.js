261974b53f4204f1351faaef4db7a930985dca51
Alfred Boateng <programmingwithalfred@gmail.com>
2018-03-14 22:53:43 +0000

    starter codes

---
+++ components/count/src/Counter.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import "./Counter.css";
+class Counter extends Component {
+    render() {
+        return (
+            <div>
+            <h1>images</h1>
+            <img src="https://images.unsplash.com/photo-1485381473713-c6131409c0be?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b4a7612415f6780eb09a06affbdcbd66&auto=format&fit=crop&w=500&q=60" alt ="image" />
+
+             <img src="https://images.unsplash.com/reserve/RNm0KceQ4Gbpb0xldOe7_DSC_0679_2.JPG?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5be73e1293b3428b75f5a8f32889bf85&auto=format&fit=crop&w=500&q=60" alt ="image" />
+
+            <img src="https://images.unsplash.com/photo-1485381473713-c6131409c0be?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=b4a7612415f6780eb09a06affbdcbd66&auto=format&fit=crop&w=500&q=60" alt ="image" />
+
+             <img src="https://images.unsplash.com/reserve/RNm0KceQ4Gbpb0xldOe7_DSC_0679_2.JPG?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=5be73e1293b3428b75f5a8f32889bf85&auto=format&fit=crop&w=500&q=60" alt ="image" />
+
+
+            </div>
+
+        );
+    }
+}
+
+export default Counter;
